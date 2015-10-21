1086382e19df0e93e68c0d05c0c791d82460d18d
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-10-21 21:53:35 +0200

    Full VBProj path

--- vbncw.vb
+++ vbncw.vb
@@ -14,7 +14,9 @@ Public Class VBNCW
     'run VBNC, check & report errors
     'run post-build tasks
     'run output executable (if a flag is present?)
-    
+
+    Shared tmpString as String = ""
+
     Public Shared Sub Main(args as String())
         If args.Length = 0 Then
             Console.Write("Enter SLN location: ")
@@ -25,19 +27,17 @@ Public Class VBNCW
         
         If Exists(tmpString)
             ParseSLN(tmpString)
+            End
         Else
             Console.WriteLine("File """ & tmpString & """ not found!")
+            End
         End If
-        Console.Readline()
-        End
-
+        
         System.Diagnostics.Process.Start("vbnc", "helloVB.vb")
         System.Threading.Thread.Sleep(500)
         System.Diagnostics.Process.Start("mono", "helloVB.exe")
         System.Threading.Thread.Sleep(100)
     End Sub
-
-    Shared tmpString as String = ""
 
     Shared Sub ParseSLN(filePath as String)
         For Each line as String In ReadLines(filePath)
@@ -45,17 +45,27 @@ Public Class VBNCW
                 tmpString = line.SubString(line.IndexOf(","))
                 tmpString = tmpString.Substring(tmpString.IndexOf("""") +1)
                 tmpString = tmpString.Remove(tmpString.IndexOf(""""))
+                tmpString = tmpString.Replace("\", "/")
+                ' tmpString now contains relative path, now get full path
+                tmpString = filePath.Remove(filePath.LastIndexOf("/") +1) & tmpString
+                
                 ParseVBProj(tmpString)
             End If
         Next
     End Sub
 
     Shared Sub ParseVBProj(filePath as string)
-        Console.WriteLine("Found VBProj at: " & filePath)
+        Console.WriteLine("File indicates VBProj at: " & filePath)
+        If Exists(filePath)
+            Console.WriteLine("File """ & filePath & """ exists!")
+            BuildProject()
+        Else
+            Console.WriteLine("File """ & filePath & """ does not exist!")
+        End If
     End Sub
 
     Shared Sub BuildProject()
-
+        
     End Sub
 
     ' Console commands:
