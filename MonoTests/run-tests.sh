e0320d325762d2b1bfd6419de4e9d6f13030532b
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-10-21 02:20:40 +0200

    Disabled ASP.Net test, moved VB test up because Gtk# test doesn't finish

--- MonoTests/run-tests.sh
+++ MonoTests/run-tests.sh
@@ -14,13 +14,14 @@ mcs helloWinForms.cs -pkg:dotnet
 mono helloWinForms.exe
 
 # ASP.Net Hello World
-xsp4 --port 9000
-curl http://localhost:9000/hello.aspx > /dev/null
-
-# Gtk# Hello World
-mcs helloGtk.cs -pkg:gtk-sharp-2.0
-mono helloGtk.exe
+#xsp4 --port 9000
+#curl http://localhost:9000/hello.aspx > /dev/null
+## Disabled because xsp4 runs in the current terminal
 
 # VB.Net Console Hello World
 vbnc helloVB.vb
 mono helloVB.exe
+
+# Gtk# Hello World
+mcs helloGtk.cs -pkg:gtk-sharp-2.0
+mono helloGtk.exe
