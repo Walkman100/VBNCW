22c8948c0961264ac071f4562fb94341be489fb9
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-10-21 18:42:45 +0200

    Added SLN parsing, it compiles but doesn't run

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
-vbnc vbncw.vb
+#!/bin/sh
+vbnc "-out:vbncw.exe" -nologo -utf8output -quiet -rootnamespace:vbncw -target:exe  "vbncw.vb"
+
+# flags:
+# -nologo -utf8output -quiet -debug:full -optionstrict- -optionexplicit+ -optioncompare:binary -optioninfer- -rootnamespace:vbncw -target:exe
--- vbncw.vb
+++ vbncw.vb
@@ -1,6 +1,6 @@
 Imports System
 
-Public Class HelloWorld
+Public Class VBNCW
     '   TO-DO
     'scan current directory for .slns or .vbprojs, or skip if a valid file is provided as a flag
     'parse .SLN (if any) to get VBProj location
@@ -16,11 +16,46 @@ Public Class HelloWorld
     'run output executable (if a flag is present?)
 
     Public Shared Sub Main()
-        Console.WriteLine("Hello Mono World")
-        'I don't even know where to start without an IDE with autocomplete :/
+        Me.UnsharedMain() 'Otherwise I have to put "Me." before every reference to local objects -_-
+    End Sub
+
+    Dim tmpString as String = ""
+
+    Sub UnsharedMain()
+        ParseSLN(Console.Readline("Enter SLN location:"))
+        Console.Readline()
+        End
+
         System.Diagnostics.Process.Start("vbnc", "helloVB.vb")
         System.Threading.Thread.Sleep(500)
         System.Diagnostics.Process.Start("mono", "helloVB.exe")
         System.Threading.Thread.Sleep(100)
     End Sub
+
+    Sub ParseSLN(filePath as String)
+        For Each line as String In System.IO.File.ReadLines(filePath)
+            If line.StartsWith("Project(", True, Nothing) Then
+                  ParseVBProj(line)
+                tmpString = line.SubString(line.IndexOf(","))
+                  ParseVBProj(tmpString)
+                tmpString = tmpString.Substring(tmpString.IndexOf(""""))
+                  ParseVBProj(tmpString)
+                tmpString = tmpString.Remove(tmpString.IndexOf(""""))
+                ParseVBProj(tmpString)
+            End If
+        Next
+    End Sub
+
+    Sub ParseVBProj(filePath as string)
+        Console.WriteLine("Found VBProj at: " & filePath)
+    End Sub
+
+    Sub BuildProject()
+
+    End Sub
+
+    ' Console commands:
+    '  Console.WriteLine("text")
+    '  text = Console.ReadLine("text")
+    '  character = Console.ReadKey("text")
 End Class
