5e9ca8b417964a76b30fd98c7c6706d62536bc25
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-11-03 00:17:53 +0200

    made run tests try all the tests even if others fail don't try running programs if compiling fails added info on closing the Gtk# program

--- MonoTests/run-tests.sh
+++ MonoTests/run-tests.sh
@@ -1,38 +1,55 @@
 #!/bin/sh
 
-die() {
+eecho() {
     echo
     echo "$* Failed!"
-    exit 1
 }
-# From the awesomeness that is https://stackoverflow.com/a/25515370/2999220
 
 # Console Hello World
-mcs hello.cs || die "Compiling hello.cs"
-mono hello.exe || die "Running hello.exe"
+if mcs hello.cs; then
+    mono hello.exe || eecho "Running hello.exe"
+else
+    eecho "Compiling hello.cs"
+fi
 echo
 
 # HTTPS connections
-wget -q https://raw.github.com/mono/mono/master/mcs/class/Mono.Security/Test/tools/tlstest/tlstest.cs || die "Downloading tlstest.cs"
-mcs tlstest.cs /r:System.dll /r:Mono.Security.dll || die "Compiling tlstest.cs"
-mono tlstest.exe https://www.nuget.org || die "Running tlstest.exe"
+if wget -nv https://raw.github.com/mono/mono/master/mcs/class/Mono.Security/Test/tools/tlstest/tlstest.cs; then
+    if mcs tlstest.cs /r:System.dll /r:Mono.Security.dll; then
+        mono tlstest.exe https://www.nuget.org || eecho "Running tlstest.exe"
+    else
+        eecho "Compiling tlstest.cs"
+    fi
+else
+    eecho "Downloading tlstest.cs"
+fi
 echo
 
 # Winforms Hello World
-mcs helloWinForms.cs -pkg:dotnet || die "Compiling helloWinForms.cs"
-mono helloWinForms.exe || die "Running helloWinForms.exe"
+if mcs helloWinForms.cs -pkg:dotnet; then
+    mono helloWinForms.exe || eecho "Running helloWinForms.exe"
+else
+    eecho "Compiling helloWinForms.cs"
+fi
 echo
 
 # VB.Net Console Hello World
-vbnc -nologo -utf8output -quiet helloVB.vb || die "Compiling helloVB.vb"
-mono helloVB.exe || die "Running helloVB.exe"
+if vbnc -nologo -utf8output -quiet helloVB.vb; then
+    mono helloVB.exe || eecho "Running helloVB.exe"
+else
+    eecho "Compiling helloVB.vb"
+fi
 echo
 
 # ASP.Net Hello World
 echo "Open http://localhost:9000/hello.aspx in your browser, once XSP4 has started, to complete this test"
+echo "Starting..."
 xsp4 --port 9000 || die "Starting ASP.Net server"
-echo
 
 # Gtk# Hello World
-mcs helloGtk.cs -pkg:gtk-sharp-2.0 || die "Compiling helloGtk.cs"
-mono helloGtk.exe || die "Running helloGtk.exe"
+if mcs helloGtk.cs -pkg:gtk-sharp-2.0; then
+    echo "The Gtk# test program doesn't exit on it's own, once you close the window you need to ^C it to kill it"
+    mono helloGtk.exe || eecho "Running helloGtk.exe"
+else
+    eecho "Compiling helloGtk.cs"
+fi
