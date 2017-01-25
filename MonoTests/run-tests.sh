2976abb94edebdd2ae98932a84da911a5acc0741
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2017-01-26 00:48:38 +0200

    Moved console tests together, fixed ASP test error handler

--- MonoTests/run-tests.sh
+++ MonoTests/run-tests.sh
@@ -13,6 +13,14 @@ else
 fi
 echo
 
+# VB.Net Console Hello World
+if vbnc -nologo -utf8output -quiet helloVB.vb; then
+    mono helloVB.exe || eecho "Running helloVB.exe"
+else
+    eecho "Compiling helloVB.vb"
+fi
+echo
+
 # HTTPS connections
 if wget -nv https://raw.github.com/mono/mono/master/mcs/class/Mono.Security/Test/tools/tlstest/tlstest.cs; then
     if mcs tlstest.cs /r:System.dll /r:Mono.Security.dll; then
@@ -33,22 +41,14 @@ else
 fi
 echo
 
-# VB.Net Console Hello World
-if vbnc -nologo -utf8output -quiet helloVB.vb; then
-    mono helloVB.exe || eecho "Running helloVB.exe"
-else
-    eecho "Compiling helloVB.vb"
-fi
-echo
-
 # ASP.Net Hello World
 echo "Open http://localhost:9000/hello.aspx in your browser, once XSP4 has started, to complete this test"
 echo "Starting..."
-xsp4 --port 9000 || die "Starting ASP.Net server"
+xsp4 --port 9000 || eecho "Starting ASP.Net server"
 
 # Gtk# Hello World
 if mcs helloGtk.cs -pkg:gtk-sharp-2.0; then
-    echo "The Gtk# test program doesn't exit on it's own, once you close the window you need to ^C it to kill it"
+    echo "The Gtk# test program doesn't exit on it's own, you need to ^C this to kill it"
     mono helloGtk.exe || eecho "Running helloGtk.exe"
 else
     eecho "Compiling helloGtk.cs"
